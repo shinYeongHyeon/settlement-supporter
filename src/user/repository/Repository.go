1fd7d2b15ab67ef4f5a966cadb9499740699331f
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-03-06 20:43:48 +0900

    Repository guard reinforce

--- src/user/repository/Repository.go
+++ src/user/repository/Repository.go
@@ -1,6 +1,7 @@
 package userRepository
 
 import (
+	"errors"
 	userDomain "github.com/shinYeongHyeon/settlement-supporter/src/user/domain"
 	userEntity "github.com/shinYeongHyeon/settlement-supporter/src/user/entity"
 	"gorm.io/gorm"
@@ -8,15 +9,23 @@ import (
 
 var userDb *gorm.DB
 
+// Init : userDb instance create
 func Init(db *gorm.DB) {
 	userDb = db
 }
 
-func Create(user userDomain.User) {
+// Create : create User Row
+func Create(user userDomain.User) error {
+	if userDb == nil {
+		return errors.New("database not initialized")
+	}
+
 	userDb.Create(&userEntity.User{
 		UUID:     user.GetUuid(),
 		Id:       user.GetId().Value(),
 		Password: user.GetPassword().Value(),
 		NickName: user.GetNickName().Value(),
 	})
+
+	return nil
 }
